09d844a2f97bf11757a4face198e6f88e233484a
Alper Ayduman <aydumanalper@gmail.com>
2020-07-18 12:01:38 +0300

    Initial commit

---
+++ Assets/Scripts/PathCreator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PathCreator : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+
+    private List<Vector3> points = new List<Vector3>();
+
+    public Action<IEnumerable<Vector3>> OnNewPathCreated = delegate { };
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+            points.Clear();
+
+        if (Input.GetButton("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray, out hitInfo)) //add layer mask for non clickable areas
+            {
+                //how far away recorded last point from new point
+                if (DistanceToLastPoint(hitInfo.point) > 1f)
+                {
+                    points.Add(hitInfo.point);
+
+                    lineRenderer.positionCount = points.Count;
+                    lineRenderer.SetPositions(points.ToArray());
+
+
+
+                }
+            }
+        }
+        else if (Input.GetButtonUp("Fire1"))
+            OnNewPathCreated(points);
+    }
+
+    private float DistanceToLastPoint(Vector3 point)
+    {
+        if (!points.Any())
+            return Mathf.Infinity;
+        return Vector3.Distance(points.Last(), point);
+    }
+}
